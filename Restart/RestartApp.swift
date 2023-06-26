8eef50df304e380660d7c27dc728673bea87284d
Deepak Kumar <40658240+kunalmanshani@users.noreply.github.com>
2023-06-26 15:11:37 +0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by kunalmanshani on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
